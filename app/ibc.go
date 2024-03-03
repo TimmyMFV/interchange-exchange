5c85907aacced3181e1857169c0c9ddb7d4c1aba
trung <trung.nguyen25@tiki.vn>
2024-03-03 14:13:00 +0700

    add dex module

--- app/ibc.go
+++ app/ibc.go
@@ -33,7 +33,10 @@ import (
 	ibckeeper "github.com/cosmos/ibc-go/v8/modules/core/keeper"
 	solomachine "github.com/cosmos/ibc-go/v8/modules/light-clients/06-solomachine"
 	ibctm "github.com/cosmos/ibc-go/v8/modules/light-clients/07-tendermint"
+
 	// this line is used by starport scaffolding # ibc/app/import
+	dexmodule "interchange/x/dex/module"
+	dexmoduletypes "interchange/x/dex/types"
 )
 
 // registerIBCModules register IBC keepers and non dependency inject modules.
@@ -156,6 +159,8 @@ func (app *App) registerIBCModules() {
 		AddRoute(icacontrollertypes.SubModuleName, icaControllerIBCModule).
 		AddRoute(icahosttypes.SubModuleName, icaHostIBCModule)
 
+	dexIBCModule := ibcfee.NewIBCMiddleware(dexmodule.NewIBCModule(app.DexKeeper), app.IBCFeeKeeper)
+	ibcRouter.AddRoute(dexmoduletypes.ModuleName, dexIBCModule)
 	// this line is used by starport scaffolding # ibc/app/module
 
 	app.IBCKeeper.SetRouter(ibcRouter)
